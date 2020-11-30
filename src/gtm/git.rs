6d87bad90c09f9d9614d867f8e961116e2467812
tavo <tavo.annus@gmail.com>
2020-11-30 21:38:07 +0200

    Get branch names

--- src/gtm/git.rs
+++ src/gtm/git.rs
@@ -1,9 +1,10 @@
 #![deny(warnings)]
 
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
-use git2::{BranchType, Error, FetchOptions, Note, RemoteCallbacks, Repository};
+use git2::{BranchType, Error, FetchOptions, Note, Oid, RemoteCallbacks, Repository};
 use git2::build::RepoBuilder;
 
 use crate::gtm::gtm;
@@ -12,6 +13,8 @@ use crate::model::config;
 static GTM_NOTES_REF: &str = "refs/notes/gtm-data";
 static GTM_NOTES_REF_SPEC: &str = "+refs/notes/gtm-data:refs/notes/gtm-data";
 static DEFAULT_ORIGIN: &str = "origin";
+static ORIGIN_PREFIX: &str = "refs/remotes/origin/";
+static ORIGIN_HEAD: &str = "refs/remotes/origin/HEAD";
 
 pub fn clone_or_open(repo_config: &config::Repository) -> Result<Repository, Error> {
     let path = Path::new(&repo_config.path);
@@ -79,7 +82,7 @@ pub fn fetch(repo: &Repository, repo_config: &config::Repository) {
         let refspec = branch.get()
             .name()
             .unwrap()
-            .strip_prefix("refs/remotes/origin/")
+            .strip_prefix(ORIGIN_PREFIX)
             .unwrap();
         if refspec != "HEAD" {
             fetch_refs.push(format!("refs/heads/{}", refspec.to_string()));
@@ -95,30 +98,40 @@ pub fn fetch(repo: &Repository, repo_config: &config::Repository) {
 
 pub fn read_commits(repo: &Repository) -> Result<Vec<gtm::Commit>, Error> {
     let mut commits: Vec<gtm::Commit> = Vec::new();
-    //let mut hashes: HashSet<String> = HashSet::new();
+    let mut branch_map: HashMap<Oid, String> = HashMap::new();
     let mut revwalk = repo.revwalk().expect("Unable to revwalk!");
-    let _sorting = revwalk.set_sorting(git2::Sort::TIME);
+    revwalk.set_sorting(git2::Sort::TOPOLOGICAL).expect("Unable to set revwalk sorting!");
+    revwalk.set_sorting(git2::Sort::REVERSE).expect("Unable to reverse revalk sorting!");
     let branches = repo.branches(Option::from(BranchType::Remote)).unwrap();
     for branch in branches {
         let (branch, _) = branch.unwrap();
         let refspec = branch.get().name().unwrap();
-        if refspec == "refs/remotes/origin/HEAD" {
+        if refspec == ORIGIN_HEAD {
             continue
         }
+        branch_map.insert(
+            branch.get().target().unwrap(),
+            refspec.strip_prefix(ORIGIN_PREFIX).unwrap().to_string(),
+        );
         let _ = revwalk.push_ref(refspec);
     }
-    // let _head = revwalk.push_glob("refs/heads/*");
+
     for commit_oid in revwalk {
         let commit_oid = commit_oid?;
         let commit = repo.find_commit(commit_oid)?;
+        let mut branch = "".to_string(); // TODO: Some more intelligent choice than last wins
+        for (oid, name) in &branch_map {
+            if repo.merge_base(commit_oid, *oid).unwrap() == commit_oid && branch != "master" {
+                branch = name.clone();
+            }
+        }
         let notes: Vec<Note> = repo.notes(Option::from(GTM_NOTES_REF))?
             .map(|n| n.unwrap())
             .filter(|n| n.1 == commit_oid)
             .map(|n| repo.find_note(Option::from(GTM_NOTES_REF), n.1).unwrap())
             .collect();
 
-        let res = gtm::parse_commit(&repo, &commit, &notes, "".to_string())?;
-        // println!("{}", &res);
+        let res = gtm::parse_commit(&repo, &commit, &notes, branch)?;
         commits.push(res);
     }
     return Result::Ok(commits);
